d2823816e2dcd2499549d354dcd08a36db6197ad
Ebates10 <emilyspbates@gmail.com>
2023-08-29 06:21:59 -0400

    initial commit

---
+++ BlackjackGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BlackjackGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Blackjack Game";
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Welcome to Blackjack The Game");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("Creator: Emily SP Bates");
+
+            Console.ReadKey();
+        }
+    }
+}
